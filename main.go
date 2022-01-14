0de3d5bb8658c3ebbbbd6bc2341d97bd8742b6f3
MOSTAIN BILLAH <bill.rassel@gmail.com>
2022-01-14 20:50:45 +0600

    client form design

--- main.go
+++ main.go
@@ -17,16 +17,27 @@ func main() {
 	myWindow.Resize(fyne.NewSize(600, 400))
 
 	name := widget.NewEntry()
-	message := widget.NewMultiLineEntry()
+	name.PlaceHolder = "Enter customer name"
+
+	mobile := widget.NewEntry()
+	mobile.PlaceHolder = "Enter customer number"
+
+	email := widget.NewEntry()
+	email.PlaceHolder = "Enter customer email"
+
+	address := widget.NewMultiLineEntry()
+	address.PlaceHolder = "Type customer address"
 
 	row1 := widget.NewFormItem("Name", name)
-	row2 := widget.NewFormItem("Message", message)
+	row2 := widget.NewFormItem("Mobile", mobile)
+	row3 := widget.NewFormItem("Email", email)
+	row4 := widget.NewFormItem("Address", address)
 
-	wform := widget.NewForm(row1, row2)
+	wform := widget.NewForm(row1, row2, row3, row4)
 	wform.SubmitText = "Save"
 	wform.OnSubmit = func() {
 		name := name.Text
-		message := message.Text
+		message := address.Text
 		myData := fmt.Sprintf(`%v = %v`, name, message)
 		dialog.NewInformation("Confirmation", myData, myWindow).Show()
 
